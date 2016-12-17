8c224c6ea069d04237fc596a6525a5f5d3e7826d
snudurupati <raghav.sreeram@gmail.com>
2016-12-17 03:38:35 -0500

    removed console print statements

--- src/main/scala/WebServer.scala
+++ src/main/scala/WebServer.scala
@@ -55,11 +55,12 @@ object WebServer {
 
     val bindingFuture = Http().bindAndHandle(route, interface, port)
 
-    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+    /*println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
     bindingFuture
       .flatMap(_.unbind()) // trigger unbinding from the port
       .onComplete(_ => system.terminate()) // and shutdown when done
+      */
   }
 
 }
