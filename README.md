16437e26eca5942540395554e520536395702a91
snudurupati <raghav.sreeram@gmail.com>
2016-12-17 00:40:58 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Akka-http-logging-starter-kit
+This example shows how to use a webhook to to receive web service to receive a POST request from API.AI 
+in the form of the response to a user query matched by intents by Google Assistant
+
+## Libraries used:
+
+This project uses :
+
+1. [Akka-http](http://doc.akka.io/docs/akka/2.4.8/scala.html)
+2. [spary](http://spray.io/)
+
+## How to Run:
+Use [Lightbend Activator](https://www.lightbend.com/activator/docs) or [sbt](http://www.scala-sbt.org/) to run this project:
+
+The Server can be started by following Command :
+    sbt run
+OR
+    activator run
---
+++ src/main/scala/WebServer.scala
@@ -0,0 +1,56 @@
+/**
+  * Created by snudurupati on 12/16/16.
+  */
+
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import spray.json.DefaultJsonProtocol._
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
+import scala.io.StdIn
+import akka.http.scaladsl.Http
+
+
+object WebServer {
+
+  case class Webhook(name: String)
+
+  // formats for unmarshalling and marshalling
+  implicit val webhookFormat = jsonFormat1(Webhook)
+
+  //def saveOrder(order: Order): Future[Done] = ???
+
+  def main(args: Array[String]) {
+
+    implicit val system = ActorSystem("my-system")
+    implicit val materializer = ActorMaterializer()
+    // needed for the future flatMap/onComplete in the end
+    implicit val executionContext = system.dispatcher
+
+    val route =
+      path("webhook") {
+        post {
+          entity(as[Webhook]) {
+            webhook => complete {
+              // do some function like -- list.add(customer)
+              s"got webhook with name ${webhook.name}"
+            }
+          }
+        } ~ get {
+              complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http webservice for Google " +
+                "Home " +
+                "webhook.</h1>"))
+            }
+      }
+
+    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+
+    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+    StdIn.readLine() // let it run until user presses return
+    bindingFuture
+      .flatMap(_.unbind()) // trigger unbinding from the port
+      .onComplete(_ => system.terminate()) // and shutdown when done
+  }
+
+}
