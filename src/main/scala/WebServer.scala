5db5da635a27a76fae7b14827c1101b1161161cf
snudurupati <raghav.sreeram@gmail.com>
2016-12-19 12:17:38 -0500

    added case classes and implicit values for unmarshalling and marshilling of the exact JSON received from api.ai

--- src/main/scala/WebServer.scala
+++ src/main/scala/WebServer.scala
@@ -2,84 +2,93 @@
   * Created by snudurupati on 12/16/16.
   */
 
-import java.util.TimeZone
-
 import akka.actor.ActorSystem
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
-import akka.http.scaladsl.marshalling.ToResponseMarshallable
-import akka.http.scaladsl.model.headers.Location
 import akka.stream.ActorMaterializer
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
 import spray.json.DefaultJsonProtocol
 import spray.json.DefaultJsonProtocol._
 import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
-import scala.io.StdIn
 import akka.http.scaladsl.Http
-import java.net._
-
-case class Webhook(query: List[String], contexts: List[String], location: Option[String], timezone: Option[String], lang: Option[String], sessionID: Option[Long])
 
 case class WebhookResponse(speech: String, displayText: String, data: Option[Array[String]], contextOut: Option[List[String]], source: String)
 
+case class Metadata(intentId: String, webhookUsed: String, intentName: String)
+case class Fulfillment(speech: String, messages: Array[String])
+case class Parameter(dummy: String)
+case class Context(name: String, parameters: String, lifespan: Int)
+case class Status(code: Int, errorType: String)
+case class Result(source: String, resolvedQuery: String, action: String, actionIncomplete: Boolean, parameters:
+Parameter, contexts: Array[Context], fulfillment: Fulfillment, metadata: Metadata, score: Int)
+
+case class Webhook(id: String, timestamp: String, result: Result, status: Status, sessionId:
+String, originalRequest: Option[String])
+
 case object WebhookResponse extends SprayJsonSupport with DefaultJsonProtocol {
-  implicit val webhookFormat = jsonFormat5(WebhookResponse.apply)
+  implicit val webhookRespFormat = jsonFormat5(WebhookResponse.apply)
 }
 
 object WebServer {
 
   // formats for unmarshalling and marshalling
-  //implicit val webhookFormat = jsonFormat6(Webhook)
+  implicit val metadataFormat = jsonFormat3(Metadata.apply)
+  implicit val fullfillmentFormat = jsonFormat2(Fulfillment.apply)
+  implicit val ststusFormat = jsonFormat2(Status.apply)
+  implicit val parameterFormat = jsonFormat1(Parameter.apply)
+  implicit val contextFormat = jsonFormat3(Context.apply)
+  implicit val resultFormat = jsonFormat9(Result.apply)
+  implicit val webhookFormat = jsonFormat6(Webhook.apply)
+  implicit val webhookRespFormat = jsonFormat5(WebhookResponse.apply)
 
-  //def saveOrder(order: Order): Future[Done] = ???
   val config = ConfigFactory.load()
+
+  def fetchQueryAnswer(query: String): WebhookResponse = {
+
+    val data = Option(Array(""))
+    val context = Option(List(""))
+    val source = "diwo"
+    val speech = query match {
+      case "how are my sales today" => "excellent! very good sales today."
+      case "what is my sales forecast for next month" => "great forecast, it will be very good"
+    }
+    val displayText = speech
+    WebhookResponse(speech, speech, data, context, source)
+
+  }
 
   def main(args: Array[String]) {
 
     implicit val system = ActorSystem("my-system")
     implicit val materializer = ActorMaterializer()
-    // needed for the future flatMap/onComplete in the end
-    implicit val executionContext = system.dispatcher
-
-    val localhost = InetAddress.getLocalHost
-    val interface = localhost.getHostAddress
-    val port = 8080
+    /* needed for the future flatMap/onComplete in the end
+    implicit val executionContext = system.dispatcher*/
 
     val route =
       path("webhook") {
         post {
-          /*entity(as[Webhook]) {
+          entity(as[Webhook]) {
             webhook => complete {
-              // do some function like -- list.add(customer)
-              s"got webhook with name ${webhook.location}"
-            }*/
-            decodeRequest{
+              // generate unique response for every matched query
+              fetchQueryAnswer(webhook.result.resolvedQuery)
+            }
+            /*decodeRequest{ //unmarshalls query JSON as a string
               entity(as[String]){ str =>
                 println(str)
                 complete{
                   WebhookResponse("hey man response!", "hey man response!",Option(Array("")),Option(List("")),
                   "diwo")
-                }
-              }
-            }
+                }*/
+          }
         } ~ get {
               complete(HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http based webservice for " +
-                "Google Assistant" +
-                "webhook.</h1>"))
+                "Google Assistant" + ".</h1>"))
             }
       }
 
-
     val bindingFuture = Http().bindAndHandle(route, config.getString("http.interface"), config.getInt("http.port"))
 
-    /*println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-    StdIn.readLine() // let it run until user presses return
-    bindingFuture
-      .flatMap(_.unbind()) // trigger unbinding from the port
-      .onComplete(_ => system.terminate()) // and shutdown when done
-      */
   }
 
 }
